b0d067818324cba4d5db60a33cacdb84de327fd5
Gisele Gomes <gselegomes@gmail.com>
2023-08-31 15:02:02 -0300

    first commit

---
+++ Cliente/src/main/java/com/example/demo/Cliente.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.io.Serializable;
+public class Cliente implements Serializable {
+	private int id;
+
+	private String nome;
+	private String email;
+	private String telefone;
+	private String salario;
+	private String conta;
+}
